a8dbb3c0ca0ed6468015a1d93e48d87761d13bdf
bcrafton <crafton.b@husky.neu.edu>
2016-06-29 01:14:10 -0400

    removing unecessary stuff

--- SOR/SOR.c
+++ SOR/SOR.c
@@ -1,99 +1,279 @@
+////////////////////////////////////////////////////////////////////////////////
+
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <CL/cl.h>
+#include <stdbool.h>
+#include "Random.h"
+#include "constants.h"
 
-#define MATRIX_DIM 3
-#define NUM_ITERS	20
+////////////////////////////////////////////////////////////////////////////////
+#define M 100
+#define N 100
+////////////////////////////////////////////////////////////////////////////////
 
-double** create_matrix_A()
+ 
+// Allocates a matrix with random float entries.
+void randomMemInit(float* data, int size)
 {
-	double** matrix = (double**)malloc(sizeof(double*) * MATRIX_DIM);
-	int i;	
-	for(i=0; i<MATRIX_DIM; i++)
-	{
-		matrix[i] = (double*)malloc(sizeof(double) * MATRIX_DIM);
-	}
-	matrix[0][0] = 3.0;
-	matrix[0][1] = -1.0;
-	matrix[0][2] = 1.0;
+   int i;
 
-	matrix[1][0] = -1.0;
-	matrix[1][1] = 3.0;
-	matrix[1][2] = -1.0;
-
-	matrix[2][0] = 1.0;
-	matrix[2][1] = -1.0;
-	matrix[2][2] = 3.0;
-
-	return matrix;
+   for (i = 0; i < size; ++i)
+   	data[i] = rand() / (float)RAND_MAX;
 }
 
-double* create_vector_b()
+long LoadOpenCLKernel(char const* path, char **buf)
 {
-	double* vector = (double*)malloc(sizeof(double) * MATRIX_DIM);
-	
-	vector[0] = -1;
-	vector[1] = 7;
-	vector[2] = -7;
+    FILE  *fp;
+    size_t fsz;
+    long   off_end;
+    int    rc;
 
-	return vector;
+    /* Open the file */
+    fp = fopen(path, "r");
+    if( NULL == fp ) {
+        return -1L;
+    }
+
+    /* Seek to the end of the file */
+    rc = fseek(fp, 0L, SEEK_END);
+    if( 0 != rc ) {
+        return -1L;
+    }
+
+    /* Byte offset to the end of the file (size) */
+    if( 0 > (off_end = ftell(fp)) ) {
+        return -1L;
+    }
+    fsz = (size_t)off_end;
+
+    /* Allocate a buffer to hold the whole file */
+    *buf = (char *) malloc( fsz+1);
+    if( NULL == *buf ) {
+        return -1L;
+    }
+
+    /* Rewind file pointer to start of file */
+    rewind(fp);
+
+    /* Slurp file into buffer */
+    if( fsz != fread(*buf, 1, fsz, fp) ) {
+        free(*buf);
+        return -1L;
+    }
+
+    /* Close the file */
+    if( EOF == fclose(fp) ) {
+        free(*buf);
+        return -1L;
+    }
+
+
+    /* Make sure the buffer is NUL-terminated, just in case */
+    (*buf)[fsz] = '\0';
+
+    /* Return the file size */
+    return (long)fsz;
 }
 
-double* SOR(int n, double** A, double* b, double w, int num_iters)
+int main(int argc, char** argv)
 {
-	int i, j;
+   int err;                            // error code returned from api calls
 
-	double* x;
-	double* prev = (double*)malloc(sizeof(double) * n);
+   cl_device_id device_id;             // compute device id 
+   cl_context context;                 // compute context
+   cl_command_queue commands;          // compute command queue
+   cl_program program;                 // compute program
+   cl_kernel kernel;                   // compute kernel
 
-	for(i=0; i<n; i++)
-	{
-		prev[i] = 0.0;
-	}
+    // OpenCL device memory for matrices
+   cl_mem d_A;
+
+   // set seed for rand()
+   srand(2014);
+ 
+   //Allocate host memory for matrices A and B
+   unsigned int size_A = M * N;
+   unsigned int mem_size_A = sizeof(float) * size_A;
+   float* h_A = (float*) malloc(mem_size_A);
+
+   Random R = new_Random_seed(RANDOM_SEED);
+   double **G = RandomMatrix(M, N, R);
+   
+   int i, j;
+   for(i=0; i<M; i++)
+   {
+	   for(j=0; j<N; j++)
+	   {
+		   h_A[i*N + j] = G[i][j];
+	   }
+   }
+   
+   printf("Initializing OpenCL device...\n"); 
+
+   cl_uint dev_cnt = 0;
+   clGetPlatformIDs(0, 0, &dev_cnt);
 	
-	int iter;
-	for(iter=0; iter<num_iters; iter++)
-	{
-		x = (double*)malloc(sizeof(double) * n);
-
-		for ( i = 0; i < n; i++ )
-		{
-			x[i] = b[i];
-			for ( j = 0; j < i; j++ )
-			{
-				x[i] = x[i] - A[j][i] * x[j];
-			}
-			for ( j = i + 1; j < n; j++ )
-			{
-				x[i] = x[i] - A[j][i] * prev[j];
-			}
-			
-			x[i] = x[i] / A[i][i];
-
-		}
-
-		for ( i = 0; i < n; i++ )
-		{
-			x[i] = ( 1.0 - w ) * prev[i] + w * x[i];
-		}
-
-		free(prev);
-		prev = x;
-	}
-
-	return x;
-}
-
-int main()
-{
-	double omega = 1.25;
-	double** A = create_matrix_A();
-	double* b = create_vector_b();
-	double* x = SOR(MATRIX_DIM, A, b, omega, NUM_ITERS);
+   cl_platform_id platform_ids[100];
+   clGetPlatformIDs(dev_cnt, platform_ids, NULL);
 	
-	int i;
-	for(i=0; i<MATRIX_DIM; i++)
-	{
-		printf("%f ", x[i]);
-	}
-	printf("\n");
+   // Connect to a compute device
+   int gpu = 1;
+   err = clGetDeviceIDs(platform_ids[1], CL_DEVICE_TYPE_ALL, 1, &device_id, NULL);
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to create a device group!\n");
+       return EXIT_FAILURE;
+   }
+
+   char* value;
+   size_t valueSize;
+   // print device name
+   clGetDeviceInfo(device_id, CL_DEVICE_NAME, 0, NULL, &valueSize);
+   value = (char*) malloc(valueSize);
+   clGetDeviceInfo(device_id, CL_DEVICE_NAME, valueSize, value, NULL);
+   printf("%d. Device: %s\n", j+1, value);
+   free(value);
+  
+   // Create a compute context 
+   context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+   if (!context)
+   {
+       printf("Error: Failed to create a compute context!\n");
+       return EXIT_FAILURE;
+   }
+
+   // Create a command commands
+   commands = clCreateCommandQueue(context, device_id, 0, &err);
+   if (!commands)
+   {
+       printf("Error: Failed to create a command commands!\n");
+       return EXIT_FAILURE;
+   }
+
+   // Create the compute program from the source file
+   char *KernelSource;
+   long lFileSize;
+
+   lFileSize = LoadOpenCLKernel("SOR.cl", &KernelSource);
+   if( lFileSize < 0L ) {
+       perror("File read failed");
+       return 1;
+   }
+
+   program = clCreateProgramWithSource(context, 1, (const char **) & KernelSource, NULL, &err);
+   if (!program)
+   {
+       printf("Error: Failed to create compute program!\n");
+       return EXIT_FAILURE;
+   }
+
+   // Build the program executable
+   err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+   if (err != CL_SUCCESS)
+   {
+       size_t len;
+       char buffer[2048];
+       printf("Error: Failed to build program executable!\n");
+       clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+       printf("%s\n", buffer);
+       exit(1);
+   }
+
+   // Create the compute kernel in the program we wish to run
+   //
+   kernel = clCreateKernel(program, "sor", &err);
+   if (!kernel || err != CL_SUCCESS)
+   {
+       printf("Error: Failed to create compute kernel!\n");
+       exit(1);
+   }
+
+   // Create the input and output arrays in device memory for our calculation
+   d_A = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, mem_size_A, h_A, &err);
+
+   if (!d_A)
+   {
+       printf("Error: Failed to allocate device memory!\n");
+       exit(1);
+   }    
+    
+   printf("Running SOR for matrix A (%dx%d)\n", M, N); 
+
+   //Launch OpenCL kernel
+   size_t localWorkSize[2], globalWorkSize[2];
+ 
+   int m = M;
+   int n = N;
+   float w = 1.25;
+ 
+   err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_A);
+   err |= clSetKernelArg(kernel, 1, sizeof(int), (void *)&m);
+   err |= clSetKernelArg(kernel, 2, sizeof(int), (void *)&n);
+   err |= clSetKernelArg(kernel, 3, sizeof(float), (void *)&w);
+
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to set kernel arguments! %d\n", err);
+       exit(1);
+   }
+ 
+   localWorkSize[0] = 16;
+   localWorkSize[1] = 16;
+   globalWorkSize[0] = 1024;
+   globalWorkSize[1] = 1024;
+ 
+   err = clEnqueueNDRangeKernel(commands, kernel, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to execute kernel! %d\n", err);
+       exit(1);
+   }
+ 
+   //Retrieve result from device
+   err = clEnqueueReadBuffer(commands, d_A, CL_TRUE, 0, mem_size_A, h_A, 0, NULL, NULL);
+
+   if (err != CL_SUCCESS)
+   {
+       printf("Error: Failed to read output array! %d\n", err);
+       exit(1);
+   }
+ 
+   //print out the results
+
+   FILE * fp;
+   fp = fopen("/scratch/crafton.b/SOR.csv", "w");
+   
+   for(i = 0; i < size_A; i++)
+   {
+      if(((i + 1) % N) == 0)
+      fprintf(fp, "%f\n", h_A[i]);
+      else
+      fprintf(fp, "%f,", h_A[i]);
+   }
+   fprintf(fp, "\n");
+   
+   fclose(fp);
+
+  
+   printf("SOR completed...\n"); 
+
+   //Shutdown and cleanup
+   free(h_A);
+ 
+   clReleaseMemObject(d_A);
+
+   clReleaseProgram(program);
+   clReleaseKernel(kernel);
+   clReleaseCommandQueue(commands);
+   clReleaseContext(context);
+
+   return 0;
 }
