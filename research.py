550d066bc90dbcdda2a1e9fa2b37405931682fb0
bcrafton <crafton.b@husky.neu.edu>
2016-07-04 00:16:39 -0400

    adding amd cpu and gpu, script fix to do every comparison

--- research.py
+++ research.py
@@ -6,62 +6,58 @@ from numpy import genfromtxt
 import matplotlib.pyplot as plt
 import time
 
-research_directory = os.getcwd() + '\\research\\'
-results_directory = os.getcwd() + '\\results\\'
+def write_csv(data, key, i, j, result_matrix):
+    path = os.path.join(analysis_directory, "csv", data[key][i] + data[key][j])
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-paths = glob.glob(research_directory + '*/*.csv')
+    full_path = os.path.join(path, key)
+    numpy.savetxt(full_path, result_matrix, delimiter=',')
 
-results = {}
+def write_histogram(data, key, i, j, result_matrix):
+    path = os.path.join(analysis_directory, "histogram", data[key][i] + data[key][j])
+    if not os.path.exists(path):
+        os.makedirs(path)
+    full_path = os.path.join(path, os.path.splitext(key)[0] + ".jpg")
+
+    plt.hist(result_matrix, bins=50)
+    plt.savefig(full_path)
+    plt.close()
+
+data_directory = os.getcwd() + '\\data\\'
+analysis_directory = os.getcwd() + '\\analysis\\'
+
+paths = glob.glob(data_directory + '*/*.csv')
+
+data = {}
 
 for path in paths:
-    # f = open(path, 'rb')
-    # reader = csv.reader(f)
-
-    '''
-    for row in reader:
-        print row
-    '''
-
-    # print os.path.basename(os.path.dirname(path))
-
     try:
-        results[os.path.basename(path)].append(os.path.basename(os.path.dirname(path)))
+        data[os.path.basename(path)].append(os.path.basename(os.path.dirname(path)))
     except:
-        results[os.path.basename(path)] = []
-        results[os.path.basename(path)].append(os.path.basename(os.path.dirname(path)))
+        data[os.path.basename(path)] = []
+        data[os.path.basename(path)].append(os.path.basename(os.path.dirname(path)))
 
-    '''
-    if results[os.path.basename(path)] is None:
-        results[os.path.basename(path)] = []
-        results[os.path.basename(path)].append(os.path.basename(os.path.dirname(path)))
-    '''
-
-    # f.close()
-
-# we cud make better practices but using order dictionaries in all cases
-# so thay way no chance of amdintel and intelamd
 
 columns = {}
 columns_inv = {}
 column_count = 0
-for key in results:
+for key in data:
     columns[key] = column_count
     columns_inv[column_count] = key
     column_count = column_count + 1
-# print(columns)
 
 rows = {}
 rows_inv = {}
 row_count = 0
-for key in results:
-    for i in range(len(results[key])):
-        row = results[key][i] + results[key][(i + 1) % len(results[key])]
-        if row not in rows.keys():
-            print(results[key][i])
-            rows[row] = row_count
-            rows_inv[row_count] = row
-            row_count = row_count + 1
-# print(rows)
+for key in data:
+    for i in range(len(data[key])):
+        for j in range(i+1, len(data[key])):
+            row = data[key][i] + data[key][j]
+            if row not in rows.keys():
+                rows[row] = row_count
+                rows_inv[row_count] = row
+                row_count = row_count + 1
 
 difference = a = [['X'] * (len(columns)+1) for i in range(len(rows)+1)]
 
@@ -70,62 +66,48 @@ for i in range(len(rows)):
 for i in range(len(columns)):
     difference[0][i+1] = columns_inv[i]
 
-def get_index(array, val):
-    array = array.flatten()
-    for i in range(len(array)):
-        if val == array[i]:
-            return i
+for key in data:
+    matrix = []
+    for i in range(len(data[key])):
+        path = os.path.join(data_directory, data[key][i], key)
+        matrix.append(genfromtxt(path, delimiter=','))
+    for i in range(len(data[key])):
+        for j in range(i+1, len(data[key])):
+            # get the result
+            result_matrix = numpy.subtract(matrix[i], matrix[j])
+            # difference matrix coordinates
+            row = data[key][i] + data[key][j]
+            col = key
+            if numpy.count_nonzero(result_matrix) != 0:
+                # print the details
+                print(row + os.path.splitext(key)[0])
 
+                tmp = numpy.absolute(result_matrix.flatten())
+                idx = tmp.argsort()[-5:]
+                indexes = numpy.unravel_index(idx, result_matrix.shape)
+                # print(indexes)
 
-for key in results:
-    data = []
-    for i in range(len(results[key])):
-        path = os.path.join(research_directory, results[key][i], key)
-        data.append(genfromtxt(path, delimiter=','))
-    largest_indexes = numpy.array([])
-    for i in range(len(results[key])):
-        result_matrix = numpy.subtract(data[i], data[(i + 1) % len(results[key])])
-        # difference matrix coordinates
-        row = results[key][i] + results[key][(i + 1) % len(results[key])]
-        col = key
-        if numpy.count_nonzero(result_matrix) != 0:
+                # set difference equal to 2
+                difference[rows[row] + 1][columns[key] + 1] = 1
 
-            # print the details
-            print(results[key][i] + results[key][(i + 1) % len(results[key])] + os.path.splitext(key)[0])
+                # get current time
+                t = time.time()
 
-            tmp = numpy.absolute(result_matrix.flatten())
-            idx = tmp.argsort()[-5:]
-            indexes = numpy.unravel_index(idx, result_matrix.shape)
-            print(indexes)
-            # largest_indexes = numpy.concatenate((largest_indexes, indexes))
+                # write the csv
+                write_csv(data, key, i, j, result_matrix)
 
-            # set difference equal to 2
-            difference[rows[row] + 1][columns[key] + 1] = 1
+                # write the histogram
+                #write_histogram(data, key, i, j, result_matrix)
 
-            # get current time
-            t = time.time()
+                print("done, time taken: " + str(time.time() - t))
 
-            # write the csv
-            path = os.path.join(results_directory, results[key][i] + results[key][(i + 1) % len(results[key])] + key)
-            numpy.savetxt(path, result_matrix, delimiter=',')
-            '''
-            # write the histogram
-            plt.hist(result_matrix, bins=50)
-            path = research_directory + results[key][i] + results[key][(i + 1) % len(results[key])] + \
-                   os.path.splitext(key)[0] + ".jpg"
-            plt.savefig(path)
-            plt.close()
-            print("done, time taken: " + str(time.time() - t))
-            '''
+            else:
+                difference[rows[row] + 1][columns[key] + 1] = 0
 
-        else:
-            difference[rows[row] + 1][columns[key] + 1] = 0
-
-    print(largest_indexes)
-
-print(difference)
-
-with open(results_directory + "output.csv", "w") as f:
+with open(analysis_directory + "output.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerows(difference)
 f.close()
+
+
+
